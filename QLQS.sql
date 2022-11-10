da55c38c93a96a12ac4d0a26eef35324c1dd9d5f
'Vẹn' <'thanhven2222@gmail.com'>
2022-11-10 20:51:01 +0700

    Vẹn add file lần 4

--- QLQS.sql
+++ QLQS.sql
@@ -2,18 +2,18 @@
 GO
 USE QuanLyQuanSu
 GO
+--Bảng tài khoản
 CREATE TABLE TaiKhoan(
 ID int IDENTITY(1,1) NOT NULL,
 TaiKhoan nvarchar(50) NOT NULL,
 MatKhau nvarchar(50) NOT NULL,
-DaiDoi int NOT NULL,
-PRIMARY KEY(TaiKhoan),
-UNIQUE(DaiDoi)
+Quyen bit NOT NULL,
+PRIMARY KEY(TaiKhoan)
 )
 --Thêm dữ liệu vào bảng Tài Khoản
 INSERT INTO TaiKhoan
 VALUES ('admin001','123123','1')
-
+--Bảng quân nhân
 CREATE TABLE QuanNhan(
 ID int IDENTITY(1,1) NOT NULL,
 MaDinhDanh nvarchar(10) NOT NULL,
@@ -24,25 +24,25 @@ NhapNgu date NOT NULL,
 GioiTinh bit NOT NULL,
 NgaySinh date NOT NULL,
 HinhAnh nvarchar(500) NULL,
-PRIMARY KEY(MaDinhDanh)
+PRIMARY KEY(MaDinhDanh),
+FOREIGN KEY (DonVi) REFERENCES DaiDoi(MaDaiDoi) 
 )
 GO
-
 --Thêm dữ liệu vào bảng Quân Nhân
 INSERT INTO QuanNhan
-VALUES('TV0303',N'Đỗ Thanh Vẹn',N'Đại Tướng',N'Đại Đội 1','01-01-2022','1','03-03-2003',''),
-('YN0101',N'Nguyễn Yến Nương',N'Đại Tá',N'Đại Đội 5','01-01-2022','0','03-03-2003',''),
-('QT0101',N'Nguyễn Quốc Thái',N'Đại Tá',N'Đại Đội 6','01-01-2022','1','03-03-2003',''),
-('CK0101',N'Nguyễn Chí Khang',N'Binh Nhì',N'Đại Đội 1','01-01-2022','1','03-03-2003',''),
-('VL0101',N'Nguyễn Văn Lợi',N'Binh Nhất',N'Đại Đội 1','01-01-2022','1','03-03-2003',''),
-('MC0101',N'Trần Mộng Cơ',N'Đại Tá',N'Đại Đội 2','01-01-2022','0','03-03-2003',''),
-('QT0201',N'Trần Quốc Toản',N'Đại Tá',N'Đại Đội 2','01-01-2022','1','03-03-2003',''),
-('KC0303',N'Nguyễn Kim Cương',N'Đại Tá',N'Đại Đội 2','01-01-2022','0','03-03-2003',''),
-('VA0305',N'Nguyễn Văn An',N'Đại Tá',N'Đại Đội 1','01-01-2022','1','03-03-2003',''),
-('KC0182',N'Nguyễn Kiên Cường',N'Đại Tá',N'Đại Đội 5','01-01-2022','1','03-03-2003',''),
-('KU8287',N'Phạm Thị Kiều Uyên',N'Thiếu Tá',N'Đại Đội 1','01-01-2022','0','03-03-2003',''),
-('LD9127',N'Trần Lam Duyên',N'Binh Nhì',N'Đại Đội 1','01-01-2022','0','03-03-2003','')
-
+VALUES('TV0303',N'Đỗ Thanh Vẹn',N'Đại Tướng','DD001','01-01-2022','1','03-03-2003',''),
+('YN0101',N'Nguyễn Yến Nương',N'Đại Tá','DD002','01-01-2022','0','03-03-2003',''),
+('QT0101',N'Nguyễn Quốc Thái',N'Đại Tá','DD002','01-01-2022','1','03-03-2003',''),
+('CK0101',N'Nguyễn Chí Khang',N'Binh Nhì','DD005','01-01-2022','1','03-03-2003',''),
+('VL0101',N'Nguyễn Văn Lợi',N'Binh Nhất','DD006','01-01-2022','1','03-03-2003',''),
+('MC0101',N'Trần Mộng Cơ',N'Đại Tá','DD002','01-01-2022','0','03-03-2003',''),
+('QT0201',N'Trần Quốc Toản',N'Đại Tá','DD002','01-01-2022','1','03-03-2003',''),
+('KC0303',N'Nguyễn Kim Cương',N'Đại Tá','DD002','01-01-2022','0','03-03-2003',''),
+('VA0305',N'Nguyễn Văn An',N'Đại Tá','DD003','01-01-2022','1','03-03-2003',''),
+('KC0182',N'Nguyễn Kiên Cường',N'Đại Tá','DD005','01-01-2022','1','03-03-2003',''),
+('KU8287',N'Phạm Thị Kiều Uyên',N'Thiếu Tá','DD001','01-01-2022','0','03-03-2003',''),
+('LD9127',N'Trần Lam Duyên',N'Binh Nhì','DD002','01-01-2022','0','03-03-2003','')
+--Bảng quân phục
 CREATE TABLE QuanPhuc(
 MaQuanPhuc nvarchar(10) NOT NULL,
 TenQuanPhuc nvarchar(50) NOT NULL,
@@ -73,7 +73,7 @@ VALUES ('AO82',N'Quân Phục Nam','1','01-01-2022','',''),
 ('A94715',N'Màn Tuyn','2','01-01-2022','',''),
 ('A94123',N'Gối Cá Nhân','2','01-01-2022','',''),
 ('A94727',N'Dây Lưng Nhỏ','2','01-01-2022','','')
-
+--Bảng nhập số lượng quân phục
 CREATE TABLE NhapQuanPhuc(
 MaBienNhan int IDENTITY(1,1) NOT NULL,
 MaQuanPhuc nvarchar(10) NOT NULL,
@@ -82,7 +82,7 @@ NgayNhap date NOT NULL DEFAULT getDate(),
 PRIMARY KEY(MaBienNhan),
 FOREIGN KEY (MaQuanPhuc) REFERENCES QuanPhuc(MaQuanPhuc) ON UPDATE CASCADE
 )
-
+--Bảng cấp quân phục cho quân nhân
 CREATE TABLE NhanQuanPhuc(
 MaBienNhan int IDENTITY(1,1) NOT NULL,
 MaDinhDanh nvarchar(10) NOT NULL,
@@ -93,13 +93,15 @@ UNIQUE(MaDinhDanh, MaQuanPhuc),
 PRIMARY KEY(MaBienNhan),
 FOREIGN KEY (MaDinhDanh) REFERENCES QuanNhan(MaDinhDanh) ON UPDATE CASCADE
 )
+--Bảng xác thực size quân phục quân nhân
 CREATE TABLE SizeQuanPhuc(
 ID int IDENTITY(1,1) NOT NULL,
 MaDinhDanh nvarchar(10) NOT NULL,
 SizeQuanPhuc nvarchar(10) NOT NULL,
 SizeGiay float NOT NULL,
 SizeMu float NOT NULL,
-PRIMARY KEY(ID)
+PRIMARY KEY(ID),
+FOREIGN KEY (MaDinhDanh) REFERENCES QuanNhan(MaDinhDanh) ON DELETE CASCADE
 )
 --Thêm dữ liệu vào bảng Size Quân Phục
 INSERT INTO SizeQuanPhuc
@@ -110,17 +112,17 @@ VALUES ('TV0303','S','42','40'),
 ('VA0305','XL','42','40'),
 ('YN0101','XL','42','40'),
 ('KU8287','S','42','40')
-
-CREATE TABLE VuTrang(MaVuTrang nvarchar(10) NOT NULL,TenVuTrang nvarchar(50) NOT NULL,MoTa nvarchar(500),UNIQUE(TenVuTrang),PRIMARY KEY(MaVuTrang))--Thêm dữ liệu vào bảng Vũ TrangINSERT INTO VuTrangVALUES('AK81727',N'Súng ngắn',N'Súng ngắn là một súng cầm tay có thể được giữ và sử dụng bằng một tay. Hai loại súng ngắn phổ biến nhất được sử dụng hiện nay là súng ngắn ổ quay và súng ngắn bán tự động (semi-auto).'),('CKC9182',N'Súng trường',N'Súng trường (tiếng Anh: Rifle) là một loại súng cá nhân gọn nhẹ với nòng súng được chuốt rãnh xoắn,có báng súng và ốp lót tay hoàn chỉnh để phục vụ mục đích bắn điểm xạ'),('TL8626',N'Súng tiểu liên',N'Súng tiểu liên là loại vũ khí cá nhân tầm gần, thuộc họ súng máy, cũng thuộc họ súngtự động tùy theo phân loại của các quốc gia.'),('SBT7162',N'Súng bắn tỉa',N'Súng bắn tỉa (còn gọi là súng ngắm) là một loại súng trường chuyên dụng cho công việc bắn các mục tiêu ở khoảng cách xa với độ chính xác cao hơn bất kỳ loại súng cầm tay nào khác.')CREATE TABLE DaiDoi(MaDaiDoi nvarchar(10) NOT NULL,MaQuanTrang nvarchar(50) NULL,MaVuTrang nvarchar(50) NULLPRIMARY KEY (MaDaiDoi))CREATE TABLE ThongKeDaiDoi (MaThongKe int IDENTITY(1,1) NOT NULL,MaDaiDoi nvarchar(10) NOT NULL,MaVuTrang nvarchar(10) NULL,SoLuongVuTrang int NULL,MaQuanTrang nvarchar(10) NULL,SoLuongQuanTrang int NULL,PRIMARY KEY (MaThongKe))--Thống kê số lượng quân nhân theo nămCREATE PROC sp_ThongKeQuanNhan
+--Bảng vũ trang
+CREATE TABLE VuTrang(MaVuTrang nvarchar(10) NOT NULL,TenVuTrang nvarchar(50) NOT NULL,MoTa nvarchar(500),UNIQUE(TenVuTrang),PRIMARY KEY(MaVuTrang))--Thêm dữ liệu vào bảng Vũ TrangINSERT INTO VuTrangVALUES('AK81727',N'Súng ngắn',N'Súng ngắn là một súng cầm tay có thể được giữ và sử dụng bằng một tay. Hai loại súng ngắn phổ biến nhất được sử dụng hiện nay là súng ngắn ổ quay và súng ngắn bán tự động (semi-auto).'),('CKC9182',N'Súng trường',N'Súng trường (tiếng Anh: Rifle) là một loại súng cá nhân gọn nhẹ với nòng súng được chuốt rãnh xoắn,có báng súng và ốp lót tay hoàn chỉnh để phục vụ mục đích bắn điểm xạ'),('TL8626',N'Súng tiểu liên',N'Súng tiểu liên là loại vũ khí cá nhân tầm gần, thuộc họ súng máy, cũng thuộc họ súngtự động tùy theo phân loại của các quốc gia.'),('SBT7162',N'Súng bắn tỉa',N'Súng bắn tỉa (còn gọi là súng ngắm) là một loại súng trường chuyên dụng cho công việc bắn các mục tiêu ở khoảng cách xa với độ chính xác cao hơn bất kỳ loại súng cầm tay nào khác.')--Bảng đại độiCREATE TABLE DaiDoi(MaDaiDoi nvarchar(10) NOT NULL,TenDaiDoi nvarchar(50) NOT NULL,PRIMARY KEY (MaDaiDoi),UNIQUE (TenDaiDoi))--Thêm dữ liệu bảng Đại ĐộiINSERT INTO DaiDoiVALUES('DD001',N'Đại Đội 1'),('DD002',N'Đại Đội 2'),('DD003',N'Đại Đội 3'),('DD004',N'Đại Đội 4'),('DD005',N'Đại Đội 5'),('DD006',N'Đại Đội 6')--Bảng vũ trang và quân trang quân độiCREATE TABLE  QuanDungDaiDoi(ID int IDENTITY(1,1) NOT NULL,MaDaiDoi nvarchar(10) NOT NULL,MaVuTrang nvarchar(10) NOT NULL,MaQuanTrang nvarchar(10) NOT NULL,PRIMARY KEY (ID),FOREIGN KEY (MaDaiDoi) REFERENCES DaiDoi(MaDaiDoi),FOREIGN KEY (MaVuTrang) REFERENCES VuTrang(MaVuTrang),FOREIGN KEY (MaQuanTrang) REFERENCES QuanPhuc(MaQuanPhuc))--Thống kê số lượng quân phục và vũ trang đại đội cóCREATE TABLE ThongKeDaiDoi (MaThongKe int NOT NULL,MaDaiDoi nvarchar(10) NOT NULL,MaVuTrang nvarchar(10) NULL,SoLuongVuTrang int NULL,MaQuanTrang nvarchar(10) NULL,SoLuongQuanTrang int NULL,PRIMARY KEY (MaThongKe),FOREIGN KEY (MaThongKe) REFERENCES QuanDungDaiDoi(ID) ON DELETE CASCADE)--Thống kê số lượng quân nhân theo nămCREATE PROC sp_ThongKeQuanNhan
 AS BEGIN
 SELECT
-YEAR(NhapNgu) Nam,
-DonVi,
+YEAR(QuanNhan.NhapNgu) Nam,
+QuanNhan.DonVi,
 COUNT(*) SoLuong
-FROM QuanNhan
+FROM QuanNhan INNER JOIN DaiDoi
+ON QuanNhan.DonVi = DaiDoi.MaDaiDoi
 GROUP BY YEAR(NhapNgu),DonVi
 END
-
 --Thống kê số lượng quân phục đã phát
 CREATE PROC sp_ThongKeQuanPhucDaPhat
 AS BEGIN
